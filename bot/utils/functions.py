c4600b3c8a37f1ed31392c4c2bcfa1ad8344bf4f
padtrack <iusetrackpad@gmail.com>
2022-08-13 23:17:55 -0700

    fix initial response not returning Message

--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -5,4 +5,5 @@ async def reply(interaction: discord.Interaction, content, **kwargs):
     if interaction.response.is_done():
         return await interaction.followup.send(content, **kwargs)
     else:
-        return await interaction.response.send_message(content, **kwargs)
+        await interaction.response.send_message(content, **kwargs)
+        return await interaction.original_response()
