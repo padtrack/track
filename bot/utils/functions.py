3ef764f56aa86c7b90b96ff3ce979ca80930337a
padtrack <iusetrackpad@gmail.com>
2022-09-11 21:05:26 -0700

    fix wrong coeff in rating

--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -16,14 +16,13 @@ def rating(battles: int, wins: int, survived: int, damage: float, exp: float):
     Adapted from
     https://wiki.wargaming.net/en/Player_Ratings_(WoT)#Personal_Rating
     """
-
     avg_damage, avg_exp = damage / battles, exp / battles
 
-    a = math.asinh(0.0015 * exp)
+    a = math.asinh(0.0015 * avg_exp)
     b = 3700 * math.asinh(0.0006 * avg_damage) + math.tanh(0.002 * battles)
     c = 3500 / (1 + math.e**16 - 31 * wins) + 1400 / (1 + math.e**8 - 27 * survived)
     d = b + c * a
-    e = math.tanh(0.00163 * battles**-1.37 * d)
+    e = math.tanh(0.00163 * battles**-0.37 * d)
     f = 540 * battles**0.37 * e
 
     return f
