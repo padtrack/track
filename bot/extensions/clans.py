1ea36b155fe3983c301094339b421b9a44aee8a3
padtrack <iusetrackpad@gmail.com>
2022-09-14 01:40:48 -0700

    add members and description to clans

--- bot/extensions/clans.py
+++ bot/extensions/clans.py
@@ -1,3 +1,6 @@
+from typing import Dict, List
+import html
+
 from discord.ext import commands, tasks
 from discord import app_commands
 import discord
@@ -12,8 +15,13 @@ class ClanEmbed(discord.Embed):
     DEFAULT_COLOR = 0xFFFFFF
     PROGRESS_FOREGROUND = "▰"
     PROGRESS_BACKGROUND = "▱"
+    DESCRIPTION_MAX_LINES = 20
 
-    def __init__(self, clan: api.FullClan):
+    def __init__(
+        self,
+        clan: api.FullClan,
+        members_data: Dict[str, List[api.ClanMemberStatistics]],
+    ):
         color = self.DEFAULT_COLOR if not clan.wows_ladder else clan.wows_ladder.color
 
         super().__init__(
@@ -21,6 +29,14 @@ class ClanEmbed(discord.Embed):
             description=f"Created: <t:{int(clan.clan.created_at.timestamp())}:D>\n"
             f"Achievements: `{len(clan.achievements)}`",
             color=color,
+        )
+
+        members = list(members_data.values())[0]
+        names = sorted(member.name for member in members)
+        self.add_field(
+            name="Members",
+            value=" • ".join(f"`{name}`" for name in names),
+            inline=True,
         )
 
         all_modifiers = set()
@@ -32,7 +48,9 @@ class ClanEmbed(discord.Embed):
             building_type = api.wg.buildings[clan.region].type_of(building_id)
             total = api.wg.buildings[clan.region].upgrades_count(building_type)
             remaining = total - building.level
-            name = building.name.replace("_", " ").title()  # TODO: look for real building names
+            name = building.name.replace(
+                "_", " "
+            ).title()  # TODO: look for real building names
 
             if building.level:
                 strings.append(
@@ -46,7 +64,30 @@ class ClanEmbed(discord.Embed):
             if building.building_id not in all_modifiers:
                 oil_remaining += building.cost
 
-        self.add_field(name="Base", value=f"Oil remaining to max: `{oil_remaining}`\n" + "\n".join(strings))
+        self.add_field(
+            name="Base",
+            value=f"Oil remaining to max: `{oil_remaining}`\n" + "\n".join(strings),
+            inline=True,
+        )
+
+        # wargaming is really bad at handling apostrophes
+        description = html.unescape(html.unescape(clan.clan.raw_description))
+        if description:
+            self.add_field(
+                name="Description", value=self.truncate(description), inline=False
+            )
+
+    def truncate(self, string: str):
+        n = self.DESCRIPTION_MAX_LINES
+        start = string.find("\n")
+
+        while start >= 0 and n > 0:
+            start = string.find("\n", start + 1)
+            n -= 1
+
+        if not n and start != -1 and start != len(string):
+            return string[:start] + "\n... (truncated)"
+        return string
 
 
 class ClansCog(commands.Cog):
@@ -80,7 +121,12 @@ class ClansCog(commands.Cog):
             await interaction.followup.send("No clans found.")
             return
 
-        await interaction.followup.send(embed=ClanEmbed(clan))
+        if not (members := await api.get_clan_members(clan.region, clan.clan.id)):
+            await interaction.followup.send("Failed to fetch clan members.")
+            return
+
+        members_data = {api.DEFAULT_BATTLE_TYPE: members}
+        await interaction.followup.send(embed=ClanEmbed(clan, members_data))
 
 
 async def setup(bot: Track):
