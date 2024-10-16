7051bb0fba2698c19c5b5aecba28fa23f77ace66
padtrack <60636294+padtrack@users.noreply.github.com>
2024-10-16 13:31:54 -0700

    add role whitelist to roll

--- bot/extensions/roll.py
+++ bot/extensions/roll.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import asyncio
 import json
 import os
@@ -43,9 +45,24 @@ class RollCog(commands.GroupCog, name="roll"):
     @app_commands.describe(
         description="Optional message appended to embed's description.",
         maximum="The maximum value (inclusive). Defaults to 100.",
+        role1="User having this OR another listed role can roll.",
+        role2="User having this OR another listed role can roll.",
+        role3="User having this OR another listed role can roll.",
+        role4="User having this OR another listed role can roll.",
+        role5="User having this OR another listed role can roll.",
     )
     @app_commands.checks.has_permissions(manage_guild=True, manage_messages=True)
-    async def start(self, interaction: discord.Interaction, description: str = "", maximum: int = 100):
+    async def start(
+        self,
+        interaction: discord.Interaction,
+        description: str = "",
+        maximum: int = 100,
+        role1: Optional[discord.Role] = None,
+        role2: Optional[discord.Role] = None,
+        role3: Optional[discord.Role] = None,
+        role4: Optional[discord.Role] = None,
+        role5: Optional[discord.Role] = None,
+    ):
         await interaction.response.defer()
 
         if maximum <= 1:
@@ -60,11 +77,18 @@ class RollCog(commands.GroupCog, name="roll"):
                 )
                 return
 
+            require = list(set(r.id for r in [role1, role2, role3, role4, role5] if r is not None))
             self.data[interaction.channel_id] = {
                 "max": maximum,
+                "roles": require,
                 "rolls": {},
             }
-            embed = discord.Embed(title="Group Roll Session", description=f"Values: `0` to `{maximum}`\n{description}")
+            embed = discord.Embed(
+                title="Group Roll Session",
+                description=f"Values: `0` to `{maximum}`\n"
+                            f"Required Roles: {' '.join(f'<@&{r}>' for r in require) if require else 'None'}\n"
+                            f"{description}",
+            )
             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
             embed.set_footer(text="Type \"roll\" to roll")
             await interaction.followup.send(embed=embed)
@@ -78,6 +102,7 @@ class RollCog(commands.GroupCog, name="roll"):
 
         async with self.lock:
             if interaction.channel_id not in self.data:
+                await interaction.followup.send("No active session?")
                 return
 
             rolls = self.data.pop(interaction.channel_id)["rolls"]
@@ -112,12 +137,21 @@ class RollCog(commands.GroupCog, name="roll"):
         async with self.lock:
             if message.channel.id in self.data:
                 data = self.data[message.channel.id]
-                if message.author.id not in data["rolls"]:
-                    roll = secrets.randbelow(data["max"] + 1)
-                    await message.reply(f"You rolled a `{roll}`.")
-                    data["rolls"][message.author.id] = roll
+                if message.author.id in data["rolls"]:
+                    return
 
-            self.save()
+                if data["roles"]:
+                    for role_id in data["roles"]:
+                        if message.author.get_role(role_id):
+                            break
+                    else:
+                        return
+
+                roll = secrets.randbelow(data["max"] + 1)
+                await message.reply(f"You rolled a `{roll}`.")
+                data["rolls"][message.author.id] = roll
+
+                self.save()
 
     def save(self):
         with open(DATA_PATH, "w") as fp:
